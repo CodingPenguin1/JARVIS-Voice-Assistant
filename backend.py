0db2c0d7e5507d332c2863a5d9fd2ecdb9237fd2
☃, the Unicode Snowman <oconnor.57@wright.edu>
2019-11-27 14:13:15 -0500

    JARVIS now runs commands

--- backend.py
+++ backend.py
@@ -3,6 +3,7 @@
 import json
 import re as reee
 import socket
+import subprocess
 
 if __name__ == '__main__':
     print("Loading commands")
@@ -15,10 +16,12 @@ if __name__ == '__main__':
     for name, data in commands.items():
         data["exact"] = reee.compile(data["exact"], reee.IGNORECASE)
         data["fuzzy"] = reee.compile(data["fuzzy"], reee.IGNORECASE)
+        data["returnParse"] = reee.compile(data["returnParse"], reee.IGNORECASE)
 
     print("Initializing server")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    # Get the first open port after 22222
     port = 22222
     while True:
         try:
@@ -42,26 +45,30 @@ if __name__ == '__main__':
             # Find the best match from the list of commands
             bestMatch = "invalid"
             bestWeight = commands["invalid"]["weight"]
+            args = []
 
             print("Got command '{}', Starting with {}->{}".format(command, bestMatch, bestWeight))
 
             # First search through for exact matches
             for name, data in commands.items():
-                if bestWeight > data["weight"] and data["exact"].match(command):
+                if bestWeight > data["weight"] and data["exact"].search(command):
                     print("{} is the new best".format(name))
                     bestMatch = name
                     bestWeight = data["weight"]
+                    args = data["exact"].search(command)
 
             # If none were found, search through the fuzzy matches
             if bestMatch == "invalid":
                 print("Could not find an exact match. Looking for a fuzzy match")
                 for name, data in commands.items():
-                    if bestWeight > data["weight"] and data["fuzzy"].match(command):
+                    if bestWeight > data["weight"] and data["fuzzy"].search(command):
                         print("{} is the new best".format(name))
                         bestMatch = name
                         bestWeight = data["weight"]
+                        args = data["fuzzy"].search(command)
 
-            print("At this point we would run the command {}".format(bestMatch))
+            print("At this point we would run the command {} with arguments {}".format(bestMatch, args.groups()))
+            subprocess.run(commands[bestMatch]["command"])
 
             conn.close()
     except Exception as e:
